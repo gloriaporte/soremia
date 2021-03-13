9d584eb438df54ad3df975e84c8a753d1f55ec87
Samira <74847100+SamiraVNS@users.noreply.github.com>
2021-03-13 16:43:20 -0300

    versão13/03
    erro em carregar múltiplos personagens e no cenário

--- js/arrayGame.js
+++ js/arrayGame.js
@@ -23,25 +23,84 @@
 * matPersonagens[indice, personagem]
 * funções para preenchimento das arrays serão de acordo com capitulo
 */
-var matCenario = [];
+
+var matCenario = [
+	[1,"https://i.ibb.co/d5qbSm4/Quarto-Aelyn.png"],
+	[2,""],
+	[3,""],
+	[4,""]
+];
     //[indice, cenario]
 	
-var matPersonagens = [];
+var matPersonagens = [
+	[0,""],
+	[1,"https://i.ibb.co/m5Gxz2R/Aelyn-20210221163022.png"],
+	[2,"https://i.ibb.co/bzLb7GS/Yerin-20210302181153.png"],
+	[3,"https://i.ibb.co/yY9g1WR/Cherry-29.png"]
+];
     //[indice, personagem]
 	
-var matHistoria = [];
+var matHistoria = [
+	[1,1,0,1,0],
+	[2,1,1,2,3],
+	[3,1,1,3,0],
+	[4,1,1,4,0],
+	[5,1,1,5,0],
+	[6,1,2,6,0],
+	[7,1,2,7,0],
+	[8,1,3,8,0],
+	[9,1,3,9,0],
+	[10,1,3,10,0],
+	[11,1,0,11,0],
+	[12,1,0,1,2],
+	[13,1,0,12,0],
+	[14,1,0,16,1],
+	[15,1,0,13,0],
+	[16,1,0,14,0],
+	[17,null,null,null,null]
+];
     //[indice, cenario, personagem, texto, comportamento]
 	
-var matTexto = [];
+var matTexto = [
+	[1,"Bem vindo a Soremia.",1],
+	[2,"Oh! As personagens!",0],
+	[3,"Aqui está Aelyn.",1],
+	[4,"Diga ola para Aelyn!",1],
+	[5,"Olá Alelyn!",0],
+	[6,"Esta Yerin.",1],
+	[7,"Oi Yerin",0],
+	[8,"E esta é Cherry",1],
+	[9,"Como você é fota!",0],
+	[10,"Obrigada!",2],
+	[11,"Você está gostando?",1],
+	[12,"Que bom!",1],
+	[13,"Que pena!",1],
+	[14,"Obrigado pelo teste, até mais!",1]
+];
 	//[indice, texto, estilo]
 
-var matBtnTexto = [];
+var matBtnTexto = [
+	[1,"sim","não",null],
+	[2,"","",""]
+];
 	//[indice, textoA, textoB, textoC]
 
-var matPontosAfeto = [];
+var matRotaPonteiro = [
+	[1,13,15,null],
+	[2,null,null,null]
+];
 	//[indice, opcaoA, opcaoB, opcaoC]
 
-var matRotaPonteiro = [];
+matMultPersonagens = [
+	[1,3,2,1,null,null],
+	[2,null,null,null,null,null]
+];
+	//[indice, per1, per2, per3, per4, per5]
+
+var matPontosAfeto = [
+	[1,2,0,null],
+	[2,null,null,null]
+];
 	//[indice, opcaoA, opcaoB, opcaoC]
 
 	
--- js/funcPlay.js
+++ js/funcPlay.js
@@ -16,10 +16,15 @@
 * pontos de afeto apenas recebe e não faz cauculo
 */
 
+//temporário
+var indice = 1;
+var pontos=0;
+
 var iCenario;
 var iPersonagem;
 var iTexto;
 var iButton;
+var iMultPersonagens;
 var txtOpcaoA;
 var txtOpcaoB;
 var txtOpcaoC;
@@ -28,66 +33,97 @@ var img2;
 var comportamento;
 var estilo;
 var btnsAtivo = false;
-var pontos;
+
+//alert("ok");
+window.onload = function(){
+    desabilitarAll();
+};
 
 function gamePlay(){
-    for(i=0;i<matHistoria.length;i++){
+    if(indice==0){indice++;}
+    for(i=0;i<=matHistoria.length;i++){
         if(indice==matHistoria[i][0]){
-			comportamento = matHistoria[i][4];
-			if(comportamento == null){
+            comportamento = matHistoria[i][4];
+            if(comportamento == null){
                 fimDeJogo();
                 return 0;
             }// fim de jogo
-			else{
+            else{
 				switch(comportamento){
-					case 0://rota padrão
-						indice++;
-						iCenario = matHistoria[i][1];
-						iPersonagem = matHistoria[i][2];
-						iTexto = matHistoria[i][3];
-						selecionarCenario(iCenario);
-						selecionarPersonagem(iPersonagem);
-						formatarTexto(iTexto);
-						document.getElementById('btnB').style.display = 'none';
-						document.getElementById('btnC').style.display = 'none';
-						document.getElementById("cenario").src = img;
-							if(estilo==2){continue;}
-							else{document.getElementById("personagem").src = img2;}
-						break;
-					case 1://retornar rota padrão
-						indice = matHistoria[i][3];
-						gamePlay(indice);
-						break;
-					//Abrir opições (ativar botões)
-					case 2: //rota simples
-						iButton = matHistoria[i][3];
-						selecionarBtn(iButton);
-						break;
+                    case 0://rota padrão
+                        carregamentoPadrão();
+                        iPersonagem = matHistoria[i][2];
+                        selecionarPersonagem(iPersonagem);
+                        document.getElementById('img-personagem-1').style.display = '';  
+						document.getElementById("img-personagem-1").src = img2;
+                        indice++;
+                        break;
+                    case 1://retornar rota padrão
+                        indice = matHistoria[i][3];
+                        gamePlay(indice);
+                        break;
+                    case 2: //Abrir opições (ativar botões)
+                        iButton = matHistoria[i][3];
+                        selecionarBtn(iButton);
+                        break;
+                    case 3: //multiplos personagens
+                        carregamentoPadrão();
+                        iMultPersonagens = matHistoria[i][2];
+                        selecionalPersonagens();
+                        indice++;
+                        break;
 					default: 
-						alert("Erro fatal, por favor comunique ao suporte");
-                }
+                    alert("Erro fatal, por favor comunique ao suporte");
+                }//switch - comportamento do sistema
+            }//else - normal
 				return 0;
-            }//switch - comportamento do sistema
         }//if - selecionar ponto do indice
     }//for - percorrer array da História
 }//Function PRINCIPAL
 
+function carregamentoPadrão(){
+    desabilitarAll();
+    //erro na matriz
+    /*
+    iCenario = matHistoria[i][1];                 
+    selecionarCenario(iCenario);
+    document.getElementsByClassName(container-main).background = img;
+    document.getElementById("cenario").src = img;
+    */
+    iTexto = matHistoria[i][3];
+    formatarTexto(iTexto);
+}
+
+function desabilitarAll(){
+    document.getElementById('btn-b').style.display = 'none';
+    document.getElementById('btn-c').style.display = 'none';
+    document.getElementById('img-personagem-5').style.display = 'none';
+    document.getElementById('img-personagem-4').style.display = 'none';
+    document.getElementById('img-personagem-3').style.display = 'none';
+    document.getElementById('img-personagem-2').style.display = 'none';
+    document.getElementById('img-personagem-1').style.display = 'none';
+    document.getElementById('tela-narrativa').style.display = 'none';
+    document.getElementById('bubble').style.display = 'none';
+}
+
 function formatarTexto(iTexto){
-	for(i=0;i<matTexto.length;i++){
+	for(i=0;i<=matTexto.length;i++){
         if(iTexto==matTexto[i][0]){
 			txtOpcaoA = matTexto[i][1];
-			estilo = matTexto[i][2];
-			switch(){
+            estilo = matTexto[i][2];
+            switch(estilo){
 				case 0: //padrão
-					document.getElementById('btnA').innerText = txtOpcaoA;
+                    document.getElementById('btn-a').innerText = txtOpcaoA;
 					break;
 				case 1: //narração
-					document.getElementById('caixa').innerText = txtOpcaoA;
-					document.getElementById('btnA').innerText = "(Continuar...)";
+                    document.getElementById('tela-narrativa').style.display = '';
+                    document.getElementById('tela-narrativa').innerText = txtOpcaoA;
+					document.getElementById('btn-a').innerText = "(Continuar...)";
 					break;
-				case 2://dialogo
-					document.getElementById('balao').innerText = txtOpcaoA;
-					document.getElementById('btnA').innerText = "(Continuar...)";
+                case 2://dialogo
+                    document.getElementById('bubble').style.display = '';
+					document.getElementById('bubble').innerText = txtOpcaoA;
+					document.getElementById('btn-a').innerText = "(Continuar...)";
 			}
 			break;
 		}//formato
@@ -95,16 +131,17 @@ function formatarTexto(iTexto){
 }//definir exibição de texto
 
 function selecionarCenario(iCenario){
-    for(i=0;i<matCenarios.length;i++){
-        if(matCenarios[i][0]==iCenario){
+    for(i=0;i<=matCenario.length;i++){
+        if(iCenario==matCenarios[i][0]){
             img = matCenarios[i][1];
+            alert("img");
             break;
         }
     }
 }//Selecionar cenario na matCenario
 
 function selecionarPersonagem(iPersonagem){
-    for(i=0;i<matPersonagens.length;i++){
+    for(i=0;i<=matPersonagens.length;i++){
         if(matPersonagens[i][0]==iPersonagem){
             img2 = matPersonagens[i][1];
             break;
@@ -112,25 +149,20 @@ function selecionarPersonagem(iPersonagem){
     }
 }//Selecionar personagem na matPersonagens
 
-function fimDeJogo(){
-    alert("Fim de Jogo!");
-    document.getElementById('34875126').innerText = "Start";
-    document.getElementById("63487154").src = "";
-    document.getElementById("36294781").src = "";
-}//encerrar jogo e zerar
-
 function selecionarBtn(iButton){
-    for(i=0;i<matBtnTexto.length;i++){
+    for(i=0;i<=matBtnTexto.length;i++){
         if(iButton == matBtnTexto[i][0]){
-			txtOpcaoA = matBtnTexto[i][1];
-			document.getElementById('btnA').innerText = txtOpcaoA;
-            txtOpcaoB = matOpcaoTexto[i][2];
-            document.getElementById('btnB').style.display = '';
-            document.getElementById('btnB').innerText = txtOpcaoB;
-			txtOpcaoC = matOpcaoTexto[i][3];
+            txtOpcaoA = matBtnTexto[i][1];
+			document.getElementById('btn-a').innerText = txtOpcaoA;
+            //txtOpcaoB = matOpcaoTexto[i][2];
+            txtOpcaoB = "não";
+            document.getElementById('btn-b').style.display = '';
+            document.getElementById('btn-b').innerText = txtOpcaoB;	
+            txtOpcaoC==null;
             if(txtOpcaoC!=null){
-				document.getElementById('btnC').style.display = '';
-				document.getElementById('btnC').innerText = txtOpcaoC;
+                txtOpcaoC = matOpcaoTexto[i][3];
+                document.getElementById('btn-c').style.display ='';
+				document.getElementById('btn-c').innerText = "Talvez";
 			}
             btnsAtivo = true;
             return 0;
@@ -138,14 +170,44 @@ function selecionarBtn(iButton){
     }
 }//ativar botões
 
+function selecionalPersonagens(){
+    for(i=0;i<=matMultPersonagens.length;i++){
+        if(iMultPersonagens == matMultPersonagens[i][0]){
+            iPersonagem = matMultPersonagens[i][1];
+            habilitarPersonagem1(iPersonagem);
+            //iPersonagem = matMultPersonagens[i][2];
+            //alert(matMultPersonagens[i][2]);
+            //habilitarPersonagem2(iPersonagem);
+            break;
+        }//if - multPersonagens
+    }//for - percorrar matMultPersonagens
+}//selecionar multiplos personagens
+
+function habilitarPersonagem1(){
+    if(iPersonagem!=null){
+        selecionarPersonagem(iPersonagem);
+        document.getElementById('img-personagem-1').style.display = '';  
+        document.getElementById("img-personagem-1").src = img2;
+    }//personagem 1
+}
+function habilitarPersonagem2(){
+    alert("ok");
+    if(iPersonagem!=null){
+        selecionarPersonagem(iPersonagem);
+        document.getElementById('img-personagem-2').style.display = '';  
+        document.getElementById("img-personagem-2").src = img2;
+    }//personagem 1
+}
+    
 function recebeBtn(alt){
+    btnsAtivo = false;
     selecionarRota(iButton,alt);
     calcularAfeto(iButton,alt);
     gamePlay(indice);
 }
 
 function selecionarRota(iButton,alt){
-    for(i=0;i<matRotaPonteiro.length;i++){
+    for(i=0;i<=matRotaPonteiro.length;i++){
         if(matRotaPonteiro[i][0]==iButton){
             indice = matRotaPonteiro[i][alt];
             return 0;
@@ -154,23 +216,31 @@ function selecionarRota(iButton,alt){
 }//mudar indice de rota
 
 function calcularAfeto(iButton,alt){
-    for(i=0;i<matPontosAfeto.length;i++){
+    for(i=0;i<=matPontosAfeto.length;i++){
         if(matPontosAfeto[i][0]==iButton){
-            pontos = matPont[i][alt];
+
+            pontos = pontos + matPontosAfeto[i][alt];
             return 0;
         }
     }
 }//fazer calculo de afinidade (afeto)
 
+function fimDeJogo(){
+    alert("Fim de Jogo!");
+    desabilitarAll();
+    document.getElementById('btn-a').innerText = "Start";
+    indice = 0;
+}//encerrar jogo e zerar
+
 function play(alt){
-            if(btnsAtivo){
-                recebeBtn(alt);
-            }
-            else{
-				gamePlay();
-			}
-        }
-		
-function btnOpcaoA() {play(1);};
-function btnOpcaoB() {play(2);};
-function btnOpcaoC() {play(3);};
+    if(btnsAtivo){
+        recebeBtn(alt);
+    }
+    else{
+        gamePlay();
+    }
+}
+
+function btnOpcaoA() {play(1);}
+function btnOpcaoB() {play(2);}
+function btnOpcaoC() {play(3);}
