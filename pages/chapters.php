02a5f5ce0df657a0c1dc172ed72422326fe1f2e2
= <=>
2021-03-31 17:09:14 -0300

    add include

---
+++ pages/chapters.php
@@ -0,0 +1,14 @@
+<!-- Include do arquivo de menu -->
+<?php include('../include/menu.php'); ?>
+
+
+        <main>
+            Conteúdo principal da página
+        </main>
+
+<!-- Include do arquivo de rodapé e scripts gerais -->
+<?php include('../include/rodape.php'); ?>
+
+</body>
+</html> 
+
---
+++ pages/game.php
@@ -0,0 +1,45 @@
+﻿<!-- Include do arquivo de menu -->
+<?php include('../include/menu.php'); ?>
+
+            <main>
+                <!-- tela de jogo -->
+                <section id="cenario" class="scenery">
+                
+                    <!-- narração -->
+                    <div class="story-container" id="tela-narrativa">
+                        <span id="txt-narrativo" class="story">Desestabilizou as oportunidades dos pretos, pardos, miscigenados, indígenas e autonomia da pauta coletiva feminista. Eu não permitirei isso.</span>
+                    </div>
+
+                    <!-- botões -->
+                    <div class="buttons-container">
+                        <button id="btn-a"><span class="txt-btn-a">Oie</span></button>
+                        <button id="btn-b"><span class="txt-btn-b">(Sair)</span></button>
+                        <button id="btn-c"><span class="txt-btn-c">(Continuar)</span></button>
+                    </div>
+
+                    <!-- falas -->
+                    <div class="bubble-container">
+                        <div class="bubble-box" id="bubble">
+                            <span id="txt-balao"> Convocado ao desejo de se autosabotar se aproveitando de uma pauta.</p>
+                        </div>
+                    </div>
+
+
+                    <!-- personagem -->
+                    <div class="char-container">
+                        <img src="../teste/personagem.png" id="img-personagem-1" class="char">
+                        <img src="../teste/personagem.png" id="img-personagem-2" class="char">
+                        <img src="../teste/personagem.png" id="img-personagem-3" class="char">
+                        <img src="../teste/personagem.png" id="img-personagem-4" class="char">
+                        <img src="../teste/personagem.png" id="img-personagem-5" class="char">
+                    </div>
+                    <!-- cenário -->
+                </section>
+            </main>
+        </div>
+        
+<!-- Include do arquivo de rodapé e scripts gerais -->
+<?php include('../include/rodape.php'); ?>
+
+</body>
+</html> 
